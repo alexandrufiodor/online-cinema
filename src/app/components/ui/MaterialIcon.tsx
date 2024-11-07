1f12244f51b2a29b014265bd467ccdf5fd9a2d58
Alexandru Fiodor <alexandru.fiodor@eligence.dev>
2024-11-07 23:34:53 +0200

    clear code

--- src/app/components/ui/MaterialIcon.tsx
+++ src/app/components/ui/MaterialIcon.tsx
@@ -3,7 +3,6 @@ import * as MaterialIcons from 'react-icons/md'
 import { MaterialIconsName } from '@/app/shared/types/icon.types'
 
 const MaterialIcon: FC<{ name: MaterialIconsName }> = ({ name }) => {
-	console.log('🚀MaterialIcon.tsx:6')
 	const IconComponent = MaterialIcons[name]
 	// return <IconComponent /> || <MaterialIcons.MdDragIndicator />
 	return <IconComponent />
