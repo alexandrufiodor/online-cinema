1e94bb2786c3fe02d39fc19ebbfa5be695481f0c
Alexandru Fiodor <alexandru.fiodor@eligence.dev>
2024-11-07 23:34:37 +0200

    create Menu navigation

--- src/app/components/ui/MaterialIcon.tsx
+++ src/app/components/ui/MaterialIcon.tsx
@@ -3,6 +3,7 @@ import * as MaterialIcons from 'react-icons/md'
 import { MaterialIconsName } from '@/app/shared/types/icon.types'
 
 const MaterialIcon: FC<{ name: MaterialIconsName }> = ({ name }) => {
+	console.log('🚀MaterialIcon.tsx:6')
 	const IconComponent = MaterialIcons[name]
 	// return <IconComponent /> || <MaterialIcons.MdDragIndicator />
 	return <IconComponent />
